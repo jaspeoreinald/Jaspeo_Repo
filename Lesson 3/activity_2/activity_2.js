6bb849c96f70d35d092c0e68d00d48f9bb9a8f30
Francine Reinald Jaspeo <reinaldjaspeo.0205@gmail.com>
2024-09-02 15:38:38 +0800

    Jaspeo, Francine Reinald - Activity 2

---
+++ Lesson 3/activity_2/activity_2.js
@@ -0,0 +1,45 @@
+// Part 1: Variable Declaration
+// Task 1: Declare Variables
+let name = "Juan";
+let age = 21;
+let isStudent = true;
+// Task 2: Print Values
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+// Part 2: Data Types
+// Task 1: Create an Object
+let person = {
+    firstName: "Juan",
+    lastName: "Dela Cruz",
+    age: 21
+};
+console.log(person);
+// Task 2: Create an Array
+let colors = ["red", "green", "blue"];
+console.log(colors[1]);
+
+// Part 3: Variable Operations
+// Task 1: Arithmetic Operations
+let num1 = 8;
+let num2 = 2;
+console.log(`Addition: ${num1 + num2}`);
+console.log(`Subtraction: ${num1 - num2}`);
+console.log(`Multiplication: ${num1 * num2}`);
+console.log(`Division: ${num1 / num2}`);
+
+// Part 4: String Operations
+// Task 1: String Concatenation
+let firstName = "John";
+let lastName = "Smith";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Part 5: Type Checking
+// Task 1: Check Data Types
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof person);
+console.log(typeof colors);
