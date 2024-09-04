bbb26482cd31be00be2ccb02966d7f3484cabc98
Francine Reinald Jaspeo <reinaldjaspeo.0205@gmail.com>
2024-09-04 13:31:47 +0800

    Jaspeo, Francine Reinald - Activity 5

---
+++ Lesson 6/activity_5/activity_5.js
@@ -0,0 +1,8 @@
+let row = 10, column = 10;
+
+for (let i = 1; i <= row; i++){
+    for(let j = 1; j <= column; j++){
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+    console.log("...");
+}
